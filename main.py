f0bbcfbe35dc6f9db0a28e83f18b2728bfb44c12
adamdvorak <adam99dvorak@gmail.com>
2026-02-04 10:12:59 +0100

    added

--- main.py
+++ main.py
@@ -2,34 +2,34 @@ import math
 
 # Vypočet obsahu kruh
 
-prumer_kruhu = 5 # průměr kruhu
-zaok_kruhu = 2 # zaokrouhleni na kolik desetinych míst
-obsah_kruhu = round(pow((prumer_kruhu / 2), 2) * math.pi, zaok_kruhu)# výpočet kruhu pomocí: S = pi * r²
+prumer_kruhu = 4 # průměr kruhu
+zaokrouhleni_kruhu = 2 # zaokrouhleni na kolik desetinych míst
+obsah_kruhu = round(pow((prumer_kruhu / 2), 2) * math.pi, zaokrouhleni_kruhu)# výpočet kruhu pomocí: S = pi * r²
 
-print("Obsah kruhu je: " + str(obsah_kruhu) + " a je zaokrouhlený na " + str(zaok_kruhu) + " desetinná místa")
+print("Obsah kruhu je: " + str(obsah_kruhu) + " a je zaokrouhlený na " + str(zaokrouhleni_kruhu) + " desetinná místa")
 # print a je zde i napsáno na kolik desetinnych mist je to zaokrouhleé
 
 
 # Vypočítání obvodu kruhu se stejnými veličinami
 
-obvod_kruhu = round(math.pi * (5 / 2) * 2, zaok_kruhu)
-print("Obvod kruhu je: " + str(obvod_kruhu) + " a je zaokrouhlený na " + str(zaok_kruhu) + " desetinná místa")
+obvod_kruhu = round(math.pi * (5 / 2) * 2, zaokrouhleni_kruhu)
+print("Obvod kruhu je: " + str(obvod_kruhu) + " a je zaokrouhlený na " + str(zaokrouhleni_kruhu) + " desetinná místa")
 
 # Vypočet objemu krychle
 
 # Výpočet povrchu kvádru
 
-v_kvadr = 3 # veličiny stran
-s_kvadr = 1 # veličiny stran
-d_kvadr = 7 # veličiny stran
-povrch_kvadr = 2 * (v_kvadr * s_kvadr + s_kvadr * d_kvadr + d_kvadr * v_kvadr) # vypocet pomocí: 2 * (v * s + s * d + d * v)
+vyska_kvadr = 3 # veličiny stran
+sirka_kvadr = 1 # veličiny stran
+delka_kvadr = 7 # veličiny stran
+povrch_kvadr = 2 * (vyska_kvadr * sirka_kvadr + sirka_kvadr * delka_kvadr + delka_kvadr * vyska_kvadr) # vypocet pomocí: 2 * (v * s + s * d + d * v)
 
 print("Povrch kvádru je: " + str(povrch_kvadr)) # a zase výpis do programu
 
 str_krychle = 4 # veličina strany
 objem_krychle = pow(str_krychle, 3) # výpočet pro objem: a na 3
 
-print("Objem krychle je: " + str(objem_krychle)) # výpis do programu
+print("Objem krychle je: " + str(objem_krychle)) # print je výpis do konzole
 
 
 # Výpočet 3 strany v trojúhelníku
@@ -39,4 +39,5 @@ b_troju = 7 # strana b
 zaok_troju = 4 # zaokrouhlení na kolik desetiných míst
 c_troju = round(math.sqrt(pow(a_troju, 2) + pow(b_troju, 2)), zaok_troju) # vypocet pomoci: odmocniny a na 2 + b na 2 a zaokrouhleni celeho vypoctu
 
+# round je zaokrouhlení, sqrt odmocnina, pow je stejné jak sqr takže mocnina
 print("Strana c v trojúhelníku je: " + str(c_troju) + " a je zaokrouhlená na " + str(zaok_troju) + " desetinná místa") # zase print
